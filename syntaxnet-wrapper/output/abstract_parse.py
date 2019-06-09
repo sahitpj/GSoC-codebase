512a2ad8c5bf5bd2ea4212c4890ff2bd3ec07bf8
sahitpj <jayakrishna.sahit@iitgn.ac.in>
2019-06-09 22:10:18 +0530

    Updated hearst patterns package | added week-2 blog post | fixed model full stop bug

--- syntaxnet-wrapper/output/abstract_parse.py
+++ syntaxnet-wrapper/output/abstract_parse.py
@@ -8,8 +8,30 @@ with open("output/abstracts_data.json") as json_file:
     abstracts_data = json.load(json_file)
 
 count = 0
-for key in list(abstracts_data.keys())[1250:]:
-    command = 'echo "{}" | syntaxnet/parser.sh > output/abstract_conll_data/{}.conll'.format(cleaned(abstracts_data[key]), cleaned(key))
+for key in ["Haggai"]:  #list(abstracts_data.keys())[1250:]
+    command = 'echo "{}" | syntaxnet/parser.sh > output/trial/{}.conll'.format(cleaned(abstracts_data[key]), cleaned(key))
     os.system(command)
     if count == 3:
         break
+
+
+'''
+Test - 1
+
+1. Asexual_reproduction (thing)
+2. Simonides_of_Ceos (place)
+3. Hawaii
+
+
+Test - 2
+
+1. Anger (thing)
+2. Osman_I (person)
+3. Lillehammer (place)
+
+Test - 3
+
+1. Amalthea_%28moon%29 (thing)
+2. David_Zindell (person)
+3. Himachal_Pradesh (place)
+'''
