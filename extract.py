c2529d6035c23df105b6b14d06ac9a7528b28328
sahitpj <jayakrishna.sahit@iitgn.ac.in>
2019-08-04 11:03:44 +0530

    web-app baslines created ex | TE-webapp complete with initial bug fixes

--- extract.py
+++ extract.py
@@ -1,33 +1,46 @@
 import re
 import json
 
-f = open('abstracts_en.ttl','r', encoding='utf-8')
-l = f.readlines()
-lines = []
-buffer = []
-for i in l:
-    if i == '\n':
-        lines.append(buffer)
-        buffer = list()
-    else:
-        buffer.append(i[:-1])
-abstracts = {}
-mode = 0
-count = 0
-for line in lines:
-    context = None
-    for sentence in line:
-        if mode == 1:
-            subjectsearch = re.search(r'<http://dbpedia.org/resource/(.*)/abstract', line[0])
-            subject = subjectsearch.group(1)
-            abstracts[subject] = context
-            mode = 0
-        matches = re.search(r'nif:isString \"\"\"(.*)\"\"\"\^\^xsd:string;', sentence)
-        if matches:
-            context = matches.group(1)
-            mode = 1
-    if count == 10:
-        break
+# f = open('abstracts_en.ttl','r', encoding='utf-8')
+# l = f.readlines()
+# lines = []
+# buffer = []
+# for i in l:
+#     if i == '\n':
+#         lines.append(buffer)
+#         buffer = list()
+#     else:
+#         buffer.append(i[:-1])
+# abstracts = {}
+# mode = 0
+# count = 0
+# for line in lines:
+#     context = None
+#     for sentence in line:
+#         if mode == 1:
+#             subjectsearch = re.search(r'<http://dbpedia.org/resource/(.*)/abstract', line[0])
+#             subject = subjectsearch.group(1)
+#             abstracts[subject] = context
+#             mode = 0
+#         matches = re.search(r'nif:isString \"\"\"(.*)\"\"\"\^\^xsd:string;', sentence)
+#         if matches:
+#             context = matches.group(1)
+#             mode = 1
+#     if count == 10:
+#         break
 
-with open('abstracts_data.json', 'w') as outfile:
-    json.dump(abstracts, outfile)
+# with open('abstracts_data.json', 'w') as outfile:
+#     json.dump(abstracts, outfile)
+from pprint import pprint
+with open('wikiData.json') as f:
+    data = json.load(f)
+
+# pprint(data['data'])
+count = 0
+for topic in data['data'][20:]:
+    print(topic['paragraphs'][0]['context'])
+    count += 1
+    print(count)
+    print('\n')
+    if count  == 10:
+        break
